2b15ac4ce9b4c0f3a19c94dfc3bee50c0d4bb4bb
dev-064 <divyanshujha112@gmail.com>
2024-12-01 01:43:32 +0530

    changed the port

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import app from './app';
 
-const PORT: number = Number(process.env.PORT) || 3001;
+const PORT: number = Number(process.env.PORT) || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
